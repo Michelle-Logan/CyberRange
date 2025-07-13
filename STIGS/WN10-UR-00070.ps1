6f0574384de7ad6b5e8720a60d6318048fd0d409
Michelle Logan <michelle.logan2@outlook.com>
2025-07-13 12:40:39 -0500

    Create WN10-UR-00070.ps1

---
+++ STIGS/WN10-UR-00070.ps1
@@ -0,0 +1,47 @@
+<#
+.SYNOPSIS
+    This PowerShell script modifies local security policy to explicitly deny network logon access to the Guests group.
+
+.NOTES
+    Author          : Michelle Logan
+    LinkedIn        : linkedin.com/in/michellelogan2/
+    GitHub          : github.com/Michelle-Logan
+    Date Created    : 2025-07-08
+    Last Modified   : 2025-07-08
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-UR-00070
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-UR-00070).ps1 
+#>
+
+# Ensure 'Guests' group is denied network access
+secedit /export /cfg C:\secpol.cfg
+
+# Read current value
+$current = (Get-Content C:\secpol.cfg) -match 'SeDenyNetworkLogonRight'
+
+# Append or update the value to include Guests SID
+if ($current.Count -eq 0) {
+    Add-Content -Path C:\secpol.cfg -Value "SeDenyNetworkLogonRight = *S-1-5-32-546"
+} elseif ($current[0] -notmatch "S-1-5-32-546") {
+    (Get-Content C:\secpol.cfg) | ForEach-Object {
+        $_ -replace '^(SeDenyNetworkLogonRight\s*=\s*)(.*)', '$1*\2,*S-1-5-32-546'
+    } | Set-Content C:\secpol.cfg
+}
+
+# Import updated policy
+secedit /configure /db secedit.sdb /cfg C:\secpol.cfg /quiet
+
+# Cleanup
+Remove-Item C:\secpol.cfg -Force
