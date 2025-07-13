1a61f97f1af2267f3d18ac5ffc7ac9a2e52d83ed
Michelle Logan <michelle.logan2@outlook.com>
2025-07-13 17:40:50 -0500

    Create WN10-SO-000080.ps1

---
+++ STIGS/WN10-SO-000080.ps1
@@ -0,0 +1,42 @@
+<#
+.SYNOPSIS
+    This PowerShell script sets the required DoD Notice and Consent Banner by configuring both the LegalNoticeCaption and LegalNoticeText registry values. 
+    This message appears at the logon screen to inform users of monitoring and authorized use policies.
+
+.NOTES
+    Author          : Michelle Logan
+    LinkedIn        : linkedin.com/in/michellelogan2/
+    GitHub          : github.com/Michelle-Logan
+    Date Created    : 2025-07-13
+    Last Modified   : 2025-07-13
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-SO-000080
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-SO-000080).ps1 
+#>
+
+$noticeText = @"
+You are accessing a U.S. Government (USG) Information System (IS) that is provided for USG-authorized use only.
+
+By using this IS (which includes any device attached to this IS), you consent to the following conditions:
+
+- The USG routinely intercepts and monitors communications on this IS for purposes including, but not limited to, penetration testing, COMSEC monitoring, network operations and defense, personnel misconduct (PM), law enforcement (LE), and counterintelligence (CI) investigations.
+- At any time, the USG may inspect and seize data stored on this IS.
+- Communications using, or data stored on, this IS are not private, are subject to routine monitoring, interception, and search, and may be disclosed or used for any USG-authorized purpose.
+- This IS includes security measures (e.g., authentication and access controls) to protect USG interests — not for your personal benefit or privacy.
+- Notwithstanding the above, using this IS does not constitute consent to PM, LE or CI investigative searching or monitoring of the content of privileged communications, or work product, related to personal representation or services by attorneys, psychotherapists, or clergy, and their assistants. Such communications and work product are private and confidential. See User Agreement for details.
+"@
+
+Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "LegalNoticeCaption" -Type String -Value "DoD Notice and Consent Banner"
+Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "LegalNoticeText" -Type String -Value $noticeText
